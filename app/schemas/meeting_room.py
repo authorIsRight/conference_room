755036bb0d9f1192c5b849e527b3831fa9350349
Илья Соболев <i4sobolev@yandex.ru>
2023-04-23 15:36:13 +0200

    fixed validantion on name

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -6,11 +6,11 @@ from pydantic import BaseModel, validator, Field
 
 
 class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., max_length=100)
+    name: str #= Field(..., max_length=100)
     description: Optional[str] 
 
     @validator('name')
     def name_no_blank_no_too_long(cls, value: str):
-        if not value['name']:
+        if not value:
             raise ValueError('Имя не должно быть пустым')
         return value
