033df07cb3a2cd51178cdfe035ef2eb1f5ba0528
Илья Соболев <i4sobolev@yandex.ru>
2023-04-29 13:16:38 +0200

    model reservation topic done

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -15,7 +15,8 @@ from app.schemas.meeting_room import (
 )
 
 from app.api.validators import check_meeting_room_exists, check_name_duplicate
-
+from app.crud.reservation import reservation_crud
+from app.schemas.reservation import ReservationDB
 
 router = APIRouter()
 
@@ -97,3 +98,18 @@ async def remove_meeting_room(
     # Замените вызов функции на вызов метода.
     meeting_room = await meeting_room_crud.remove(meeting_room, session)
     return meeting_room
+
+
+@router.get(
+    '/{meeting_room_id}/reservations',
+    response_model=list[ReservationDB]
+)
+async def get_reservations_for_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    await check_meeting_room_exists(meeting_room_id, session)
+    reservations = await reservation_crud.get_future_reservations_for_room(
+        room_id=meeting_room_id, session=session
+    )
+    return reservations
--- app/schemas/reservation.py
+++ app/schemas/reservation.py
@@ -1,12 +1,20 @@
 # app/schemas/reservation.py
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from pydantic import BaseModel, root_validator, validator, Extra
+from pydantic import BaseModel, root_validator, validator, Extra, Field
+
+FROM_TIME = (
+    datetime.now() + timedelta(minutes=10)
+).isoformat(timespec='minutes')
+
+TO_TIME = (
+    datetime.now() + timedelta(hours=1)
+).isoformat(timespec='minutes')
 
 
 class ReservationBase(BaseModel):
-    from_reserve: datetime
-    to_reserve: datetime
+    from_reserve: datetime = Field(..., example=FROM_TIME)
+    to_reserve: datetime = Field(..., example=TO_TIME)
 
     class Config:
         extra = Extra.forbid
