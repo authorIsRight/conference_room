e1f63238c6d4f27800cc86d621faff802a15a6f5
Илья Соболев <i4sobolev@yandex.ru>
2023-04-29 16:29:04 +0200

    Настройки библиотеки FastAPI Users

--- app/core/config.py
+++ app/core/config.py
@@ -6,9 +6,10 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     description: str = 'Тестируем йобта'
     database_url: str
+    secret: str = 'SECRET'
 
     class Config:
         env_file = '.env'
 
 
-settings = Settings() 
+settings = Settings()
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 # app/models/__init__.py
-from .meeting_room import MeetingRoom
-from .reservation import Reservation
-from .user import User
+from .meeting_room import MeetingRoom # noqa
+from .reservation import Reservation # noqa
+from .user import User # noqa
